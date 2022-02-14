9218825a6a3c42a47dc3a9ae2ae1a9f064735bd0
clarrie-w <clarriewang@hotmail.com>
2022-02-14 16:48:40 -0500

    example double solenoid and pneumatics classes

--- src/main/java/frc/robot/subsystems/PneumaticsIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/PneumaticsIntakeSubsystem.java
@@ -5,7 +5,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.hal.CTREPCMJNI;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -14,18 +13,22 @@ public class PneumaticsIntakeSubsystem extends SubsystemBase {
   Compressor compressor;
 
   public PneumaticsIntakeSubsystem() {
-    compressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
-    compressor.setClosedLoopControl(0, true); // pump air
+    compressor = new Compressor(PneumaticsModuleType.CTREPCM);
+    compressor.enableDigital();
   }
 
-  public void pumpAirOn() {
-    compressor.setClosedLoopControl(true);
+  public void pumpAir() {
+    compressor.enableDigital();
   }
 
-  public void pumpAirOff() {
-    compressor.setClosedLoopControl(false);
+  public void stopAir() {
+    compressor.disable();
   }
 
+  public boolean isEnabled() {
+    return compressor.enabled();
+  }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
