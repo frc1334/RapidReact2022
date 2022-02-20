8ef0954f67ed8caa24804adc4506fef0d9270e43
Connor4699 <93537866+Connor4699@users.noreply.github.com>
2022-02-19 21:03:55 -0500

    Add files via upload

---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,47 @@
+package frc.robot.subsystems;
+
+import frc.robot.RobotMap;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motion.MotionProfileStatus;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimberSubsystem extends SubsystemBase{
+    TalonFX LeftClimbMotor = new TalonFX(RobotMap.LeftClimbMotor);
+    TalonFX RightClimbMotor = new TalonFX(RobotMap.RightClimbMotor);
+
+
+//public void whenPressed(ClimberRelease)    
+    public void ClimberRelease(TalonFX leftClimbMotor, TalonFX rightClimbMotor) {
+        
+        //this might not be necessary
+        RightClimbMotor.set(ControlMode.Follower, RobotMap.LeftClimbMotor);
+        
+        //set motors to neutral so spring can expand (coast)
+        LeftClimbMotor.setNeutralMode(null);
+        RightClimbMotor.setNeutralMode(null);
+
+        boolean Latching = true;
+
+        while (Latching) {
+        
+        //get encoder value
+        double MotorPos = LeftClimbMotor.getPosition();
+        
+        //stop motors once they reach target distance (or set them to hold position)
+            if (MotorPos > 180) {
+                Latching = false;
+                LeftClimbMotor.stopMotor();
+                RightClimbMotor.stopMotor();
+            }
+        }
+        
+        
+        
+
+    } 
+
+
+}
