577d95e4d38d277070f8eb54214a4cb966f8c72f
clarrie-w <clarriewang@hotmail.com>
2022-02-27 20:14:22 -0500

    Comments in PneumaticsSubsystem

--- src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
+++ src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
@@ -2,6 +2,15 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/*-------------------------------------------------------------------------
+
+Author: Clarrie Wang                                     Date: Feb 18 2022
+
+PneumaticsSubsystem class. Cylinders attached to Intake to allow it to be lowered and retracted. 
+Controlled by double solenoids: https://dylan-frc-docs.readthedocs.io/en/latest/docs/software/actuators/solenoids.html
+
+---------------------------------------------------------------------------*/
+
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
