8b3f539fde58343719f8d6b48354e95ffde73e09
willkozlowski <iskozlowskwil@gmail.com>
2022-03-03 15:37:49 -0500

    changed motor controller on indexersubsystem

--- src/main/java/frc/robot/commands/IndexerCommand.java
+++ src/main/java/frc/robot/commands/IndexerCommand.java
@@ -1,9 +1,18 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+/*
+
+Created by: William Kozlowski
+
+Created 2022-02-20
+
+Modified 2022-03-03, by William Kozlowski
+
+Indexer Subsystem, helps to sort/push the ball to fit into the conveyer, and preps the ball to be shot
+
+*/
 
 package frc.robot.commands;
 
--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -8,7 +8,7 @@ Created by: William Kozlowski
 
 Created 2022-02-10
 
-Modified 2022-02-14, by William Kozlowski
+Modified 2022-03-03, by William Kozlowski
 
 Indexer Subsystem, helps to sort/push the ball to fit into the conveyer, and preps the ball to be shot
 
@@ -24,26 +24,27 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 
 public class IndexerSubsystem extends SubsystemBase {
   /** Creates a new IndexerSubsystem. */
 
   //replace TalonFX with whatever motor controller we're using
-  TalonSRX FeederMotor1;
-  TalonSRX FeederMotor2;
+  TalonSRX IndexerMotor1;
+  TalonFX IndexerMotor2;
 
   public IndexerSubsystem() {
     //FeederMotor is set to channel ID 5 in RobotMap.java for now
-    FeederMotor1 = new TalonSRX(RobotMap.FeederMotor1);
-    FeederMotor2 = new TalonSRX(RobotMap.FeederMotor2);
+    IndexerMotor1 = new TalonSRX(RobotMap.FeederMotor1);
+    IndexerMotor2 = new TalonFX(RobotMap.FeederMotor2);
   }
 
 
   public void setIndexerVoltage (double voltage) {
     //varible ControlMode is pulled from the "com.ctre.phoenix.motorcontrol.ControlMode" import
-    FeederMotor1.set(ControlMode.PercentOutput, voltage);
-    FeederMotor2.set(ControlMode.PercentOutput, voltage);
+    IndexerMotor1.set(ControlMode.PercentOutput, voltage);
+    IndexerMotor2.set(ControlMode.PercentOutput, voltage);
   }
 
   //@Override
