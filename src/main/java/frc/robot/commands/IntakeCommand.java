05a1445500a0e06f809540a33cb45c990b3d3c0c
clarrie-w <clarriewang@hotmail.com>
2022-03-04 15:44:45 -0500

    intake changes

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -26,7 +26,9 @@ public class IntakeCommand extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    Robot.IntakeSubsystem.setPercentOutput(0);
+  }
 
   // Returns true when the command should end.
   @Override
