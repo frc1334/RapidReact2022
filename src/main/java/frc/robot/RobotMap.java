0c32b97da2ace18056b9732cf16be6a64f89af34
Zcarat <74550632+Zcarat@users.noreply.github.com>
2022-03-08 15:52:12 -0500

    labeled motors to match tuner

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -32,15 +32,15 @@ public class RobotMap {
 
   // Launcher: 2 Falcons
   // Launcher1: left; Launcher2: right; Launcher3: feeder wheel 
-  public static final int Launcher1 = 7;
-  public static final int Launcher2 = 8;
-  public static final int Launcher3 = 5;
+  public static final int LauncherLeft = 7;
+  public static final int LauncherRight = 8;
+  public static final int LauncherSmallWheel = 5;
 
   //added by William Kozlowski for the IndexerSubsystem.java
   // Indexer1: higher indexer
   // Indexer2: lower indexer
-  public static final int IndexerMotor1 = 9;
-  public static final int IndexerMotor2 = 6;
+  public static final int HigherIndexer = 9;
+  public static final int LowerIndexer = 6;
 
 }
 
--- src/main/java/frc/robot/subsystems/LauncherFXSubsystem.java
+++ src/main/java/frc/robot/subsystems/LauncherFXSubsystem.java
@@ -22,45 +22,45 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 // Talon FX is the small motor
 public class LauncherFXSubsystem extends SubsystemBase{
       // The Launcher Talons; 1 is left and 2 is right.
-  TalonFX Launcher1;
-  TalonFX Launcher2;
+  TalonFX LauncherLeft;
+  TalonFX LauncherRight;
 
   public LauncherFXSubsystem() {
 
-    Launcher1 = new TalonFX(RobotMap.Launcher1);
-    Launcher2 = new TalonFX(RobotMap.Launcher2);
+    LauncherLeft = new TalonFX(RobotMap.LauncherLeft);
+    LauncherRight = new TalonFX(RobotMap.LauncherRight);
     
     // Configure the default Integrated Sensors for the Talons
-    Launcher1.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 0);
-    Launcher2.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 0);
+    LauncherLeft.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 0);
+    LauncherRight.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 0);
 
     // Set the second Talon to follow the first Talon
-    Launcher2.set(ControlMode.Follower, RobotMap.Launcher1);
+    LauncherRight.set(ControlMode.Follower, RobotMap.LauncherLeft);
     
     // Invert the first Talon (Left and Right can now run the same Velocity PID RPM)
-    Launcher1.setInverted(true);
+    LauncherLeft.setInverted(true);
 
     // Configure the peak output (max in magnitude both forwards and reverse) for the first Talon
-    Launcher1.configPeakOutputForward(1);
-    Launcher1.configPeakOutputReverse(-1);
-    Launcher2.configPeakOutputForward(1);
-    Launcher2.configPeakOutputReverse(-1);
+    LauncherLeft.configPeakOutputForward(1);
+    LauncherLeft.configPeakOutputReverse(-1);
+    LauncherRight.configPeakOutputForward(1);
+    LauncherRight.configPeakOutputReverse(-1);
 
   }
 
   // This void method sets a velocity PID setpoint on the Talons
   public void setLauncherVelocity (double setpoint) {
     // Set the first Talon's PID target to the setpoing (second Talon will follow)
-    Launcher1.set(ControlMode.Velocity, setpoint);
-    Launcher2.set(ControlMode.Velocity, setpoint);
-    System.out.println("LAUNCHER: " + Launcher1.getSelectedSensorVelocity());
+    LauncherLeft.set(ControlMode.Velocity, setpoint);
+    LauncherRight.set(ControlMode.Velocity, setpoint);
+    System.out.println("LAUNCHER: " + LauncherLeft.getSelectedSensorVelocity());
   }
 
   // This void method manually sets the percent power input to the Launcher Falcons
   public void setLauncherPercent (double percent) {
-    Launcher1.set(ControlMode.PercentOutput, percent);
-    Launcher2.set(ControlMode.PercentOutput, percent);
-    System.out.println("LAUNCHER: " + Launcher1.getSelectedSensorVelocity());
+    LauncherLeft.set(ControlMode.PercentOutput, percent);
+    LauncherRight.set(ControlMode.PercentOutput, percent);
+    System.out.println("LAUNCHER: " + LauncherLeft.getSelectedSensorVelocity());
   }
 
 
