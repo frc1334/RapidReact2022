170f4e7d4cb169a1999e9542dc12fc51757bff7b
Connor4699 <93537866+Connor4699@users.noreply.github.com>
2022-03-10 21:13:22 -0500

    Update ClimberSubsystem.java

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,70 +1,72 @@
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import frc.robot.RobotMap;
-// import com.revrobotics.CANSparkMax;
-// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.robot.RobotMap;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+
+// import com.ctre.phoenix.motion.MotionProfileStatus;
+// import com.ctre.phoenix.motorcontrol.ControlMode;
+// import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 // import com.ctre.phoenix.motorcontrol.NeutralMode;
-// import com.revrobotics.RelativeEncoder;
-
-// //unused imports
-// // import com.ctre.phoenix.motion.MotionProfileStatus;
-// // import com.ctre.phoenix.motorcontrol.ControlMode;
-// // import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 
 
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-// public class ClimberSubsystem extends SubsystemBase{
-//     CANSparkMax LeftClimbMotor = new CANSparkMax(RobotMap.LeftClimbMotor, MotorType.kBrushless);
-//     CANSparkMax RightClimbMotor = new CANSparkMax(RobotMap.RightClimbMotor, MotorType.kBrushless);
-//     // RelativeEncoder C1encoder = LeftClimbMotor.getEncoder();
-//     // RelativeEncoder C2encoder = RightClimbMotor.getEncoder();
-//     RelativeEncoder m_encoder;
-//         /**
-//      * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-//      * in the SPARK MAX to their factory default state. If no argument is passed, these
-//      * parameters will not persist between power cycles
-//      */
-//     LeftClimbMotor.restoreFactoryDefaults();
+public class ClimberSubsystem extends SubsystemBase{
+    CANSparkMax LeftClimbMotor = new CANSparkMax(RobotMap.LeftClimbMotor, MotorType.kBrushless);
+    CANSparkMax RightClimbMotor = new CANSparkMax(RobotMap.RightClimbMotor, MotorType.kBrushless);
+    // RelativeEncoder C1encoder = LeftClimbMotor.getEncoder();
+    // RelativeEncoder C2encoder = RightClimbMotor.getEncoder();
+    RelativeEncoder Left_encoder;
+    RelativeEncoder Right_encoder;
+        /**
+     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+     * in the SPARK MAX to their factory default state. If no argument is passed, these
+     * parameters will not persist between power cycles
+     */
+    // LeftClimbMotor.restoreFactoryDefaults();
 
-//     /**
-//     * In order to read encoder values an encoder object is created using the 
-//     * getEncoder() method from an existing CANSparkMax object
-//     */
+    /**
+    * In order to read encoder values an encoder object is created using the 
+    * getEncoder() method from an existing CANSparkMax object
+    */
     
-// //public void whenPressed(ClimberRelease)    
-//     public void ClimberRelease(CANSparkMax leftClimbMotor, CANSparkMax rightClimbMotor) {
+//public void whenPressed(ClimberRelease)    
+    public void ClimberRelease(CANSparkMax leftClimbMotor, CANSparkMax rightClimbMotor) {
         
-//         C1encoder.setPosition(0);
-//         C2encoder.setPosition(0);
-//         //RightClimbMotor.set(ControlMode.Follower, RobotMap.LeftClimbMotor);
+        // C1encoder.setPosition(0);
+        // C2encoder.setPosition(0);
+        //RightClimbMotor.set(ControlMode.Follower, RobotMap.LeftClimbMotor);
         
-//         //set motors to neutral so spring can expand (coast)
-//         LeftClimbMotor.setMotorType(MotorType.kBrushed);
-//         RightClimbMotor.setNeutralMode(NeutralMode.Coast);
+        //set motors to neutral so spring can expand (coast)
+        // RightClimbMotor.setNeutralMode(NeutralMode.Coast);
+        RightClimbMotor.setIdleMode(IdleMode.kCoast);
+        LeftClimbMotor.setIdleMode(IdleMode.kCoast);
+        Left_encoder = LeftClimbMotor.getEncoder();
+        Right_encoder = RightClimbMotor.getEncoder();
+        boolean Latching = true;
 
-//         boolean Latching = true;
+        while (Latching) {
+        
+        //check encoder value to make sure it doesn't go too far
+        // double MotorPos = LeftClimbMotor.getSelectedSensorPosition();
+        // double MotorPos = m_encoder.getPosition();
 
-//         while (Latching) {
-        
-//         //check encoder value to make sure it doesn't go too far
-//         // double MotorPos = LeftClimbMotor.getSelectedSensorPosition();
-//         m_encoder = LeftClimbMotor.getEncoder();
-//         double MotorPos = m_encoder.getPosition();
-//         //stop motors once they reach target distance (or set them to hold position)
-//         //4096 encoder counts in a single revoloution of CANSpark    
-//         if (m_encoder > 180) {
-//                 Latching = false;
-//                 // LeftClimbMotor.setNeutralMode(NeutralMode.Brake);
-//                 // RightClimbMotor.setNeutralMode(NeutralMode.Brake);
-//             }
-//         }
+        //stop motors once they reach target distance (or set them to hold position)
+        //4096 encoder counts in a single revoloution of CANSpark    
+        if (Left_encoder.getPosition() >= 2048 || Right_encoder.getPosition() >= 2048 ) {
+                Latching = false;
+                // LeftClimbMotor.setNeutralMode(NeutralMode.Brake);
+                // RightClimbMotor.setNeutralMode(NeutralMode.Brake);
+                RightClimbMotor.setIdleMode(IdleMode.kBrake);
+                LeftClimbMotor.setIdleMode(IdleMode.kBrake);
+            }
+        }
         
         
-//     }
-// 	public ClimberSubsystem(RelativeEncoder m_encoder) {
-// 		this.m_encoder = m_encoder;
-// 	} 
+    }
 
 
-// }
+}
